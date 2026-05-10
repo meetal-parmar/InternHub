ee9f005d06a0763fb2572ef1933b3436d37437ac
meetal parmar <meetalparmar1403@gmail.com>
2026-05-10 11:08:26 +0530

    trigger redeploy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/Home.css";
-
+//
 export default function Home() {
   return (
     <div className="home-container">
