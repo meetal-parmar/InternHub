389a62505055868407278a4036c8658b3fe5cdda
meetal parmar <meetalparmar1403@gmail.com>
2026-05-10 11:49:08 +0530

    updated cors for vercel frontend

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,7 @@ mongoose.connect(process.env.MONGO_URL)
   .catch((err) => console.log(err));
 
 // Middleware
-//app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-//
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://intern-hub-git-feature-auth-meetal-parmars-projects.vercel.app"
